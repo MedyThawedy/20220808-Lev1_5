54995baa0a0ae2a08790188c53bcd8300bf017d2
MD <dhmedy@googlemail.com>
2022-08-08 15:09:37 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+// Definiere ein Array songs, das folgendes enthält:
+// Bohemian Rhapsody, Stairway to Heaven, Hotel California
+
+
+let songs = ["Bohemian Rhapsody", "Stairway to Heaven", "Hotel California"];
+console.log(songs);
+
+// Füge diesem Array deine 3 Lieblingslieder mit dem Befehl songs.push("Sweet Child O'Mine"); hinzu.
+songs.push("Sweet Child O'Mine");
+console.log(songs);
+
+songs.push("Californication");
+console.log(songs);
+
+songs.push("Smells like ten spirits");
+console.log(songs);
+
+// Speichere das Array als Variable totalSongs.
+let totalSongs = songs;
+
+// Gebe totalSongs in der Konsole aus.
+console.log(totalSongs);
+
+// Definiere ein Array besteFussballerAllerZeiten, das folgendes enthält:
+
+// die 5 besten Fußballspieler.
+let besteFußballer = ["Pelé", "Maradonna", "Ronaldo", "Batistuta", "Messi"];
+console.log(besteFußballer);
+
+// Füge 3 Torhüter hinzu.
+let dreiTorhueter = ["Barthez", "Kahn", "Higuita"];
+
+// Gebe es in der Konsole aus.
+console.log(dreiTorhueter);
+
+// Du kannst mehrere Elemente zu Deinem Array hinzufügen.
+// Verwende das Komma zwischen den Werten.
+const artists = ["michael", "elvis"];
+artists.push("justin", "charlie");
+
+// Füge Songs zu Array hinzu, die dir nicht gefallen.
+totalSongs.push("First song", "Second song");
+console.log(totalSongs);
+
+// Füge die besten 3 Trainer aller Zeiten zu Array hinzu.
+let trainer = ["Moreno", "Rijkaard", "Beckenbauer"];
+console.log(trainer);
+
+// Arrays können auch Arrays enthalten.
+// Definiere eine Variable heroUndEnemy, die 3 Arrays beinhaltet:
+// Batman, The Joker Professor X, Magneto Thor, Loki
+let heroUndEnemy = [["Batman", "The Joker"], ["Professor X", "Magneto"], ["Thor", "Loki"]];
+
+// Füge zum Array andere 3 Arrays mit Hero und ihr Gegner hinzu.
+heroUndEnemy.push(["Hero 1", "Gegner 1"]);
+heroUndEnemy.push(["Hero 2", "Gegner 2"]);
+heroUndEnemy.push(["Hero 3", "Gegner 3"]);
+// Gebe heroUndEnemy in der Konsole aus.
+console.log(heroUndEnemy);
